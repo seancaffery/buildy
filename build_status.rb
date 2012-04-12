7b26d040685c7dbc8640d6d1e472dd8c93397e15
Sean Caffery <sean@lineonpoint.com>
2012-04-12 22:13:47 +1000

    add update_revision action

--- build_status.rb
+++ build_status.rb
@@ -4,11 +4,22 @@ require 'json'
 
 require 'models'
 
-get '/' do
-end
+class BuildStatus #< Sinatra::Base
+  get '/' do
+    @revisions = Revision.all
+  end
 
-get '/last_good_revision' do
-end
+  get '/last_good_revision' do
+  end
 
-post '/update_build' do
+  post '/update_build' do
+    build_info = JSON.parse(params[:payload])
+
+    revision = Revision.find_or_create_by_revision_id(build_info['revision_id'])
+    build = Build.find_by_name(build_info['build_name'])
+    revision.builds << build if revision.new_record?
+
+    revision.build_results.build(:build => build, :result => build_info['revision_id'])
+    revision.save!
+  end
 end
