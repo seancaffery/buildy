fc1d14aac35c8b9d184eccbdb99538315ca094ea
Sean Caffery <sean@lineonpoint.com>
2012-04-24 21:08:24 +1000

    add scaffold views

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,11 @@ BuildStatus::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  #
+  resources :branches do
+    resources :builds
+    resources :revisions
+  end
 
   # Sample resource route with options:
   #   resources :products do
