27e3202e921d7d0cd965dba8716843c3bcd3414b
Sean Caffery <sean.caffery@c3businesssolutions.com>
2012-08-19 22:11:19 +1000

    add unkown state to revision

--- app/models/revision.rb
+++ app/models/revision.rb
@@ -4,19 +4,29 @@ class Revision < ActiveRecord::Base
 
   attr_accessible :branch_id, :sha
 
+  GOOD = 'good'
+  BAD = 'bad'
+  UNKOWN = 'unkown'
+
   def good?
+    status == GOOD
+  end
+
+  def status
+    return @status if @status
+
     builds = branch.builds(:conditions => { :enabled => true} )
     branch_build_names = builds.collect(&:name)
     revisions = results_for(builds)
     revision_build_names = revisions.collect(&:build).collect(&:name)
 
-    return false if builds.empty?
-    return false unless branch_build_names.sort == revision_build_names.sort
+    return @status = UNKOWN if builds.empty?
+    return @status = UNKOWN unless branch_build_names.sort == revision_build_names.sort
 
     results = revisions.map(&:result)
-    return false if results.include? 'FAILURE'
+    return @status = BAD if results.include? 'FAILURE'
 
-    true
+    @status = GOOD
   end
 
   protected
