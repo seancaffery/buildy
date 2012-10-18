b8684bf66d2e03897017ff3cff47110c66a1f3e5
Sean Caffery <sean.caffery@c3businesssolutions.com>
2012-10-18 23:09:20 +1100

    add update build smoke test

---
+++ spec/requests/update_build_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+require 'json'
+
+describe "Update build" do
+  context "branch and build exist" do
+    it "creates a build result with given data" do
+      branch = Branch.create(:name => 'master')
+      branch.builds.create(:name => 'lol')
+      build_info = {'branch' => 'master', 'build_name' => 'lol',
+                    'revision_id' => 'sha', 'result' => 'SUCCESS',
+                    'duration' => '30000'}
+
+      post 'update_build', :payload => JSON.dump(build_info)
+
+      response.status.should == 200
+      result = BuildResult.last
+      result.result.should == 'SUCCESS'
+      result.build_time.should == 30000
+    end
+  end
+
+  context "build does not exist" do
+    it "responds with unprocessable entity" do
+      build_info = {'branch' => 'master', 'build_name' => 'lol',
+                    'revision_id' => 'sha', 'result' => 'SUCCESS',
+                    'duration' => '30000'}
+
+      post 'update_build', :payload => JSON.dump(build_info)
+      response.status.should == 422
+    end
+  end
+end
